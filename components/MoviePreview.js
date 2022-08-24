9b9efe505d01b8cd5405544f74c4b241382d9f4a
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2022-08-25 04:07:48 +0530

    [FEAT]: added pagination

--- components/MoviePreview.js
+++ components/MoviePreview.js
@@ -1,12 +1,47 @@
 import React from "react";
-import { View, Text } from "react-native";
+
+import {
+  View,
+  Text,
+  ImageBackground,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
 
 const MoviePreview = ({ movie, onPress }) => {
   return (
-    <View>
-      <Text>{movie.Title}</Text>
-    </View>
+    <TouchableOpacity>
+      <View style={styles.container}>
+        <ImageBackground source={{ uri: movie.Poster }} style={styles.image}>
+          <Text>{movie.Title}</Text>
+        </ImageBackground>
+      </View>
+    </TouchableOpacity>
   );
 };
 
 export default MoviePreview;
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 40,
+    marginVertical: 10,
+  },
+
+  heading: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  image: {
+    height: 140,
+    width: 240,
+    resizeMode: "cover",
+    justifyContent: "center",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.3,
+    shadowRadius: 1,
+    elevation: 2,
+  },
+});
--- screens/Home.js
+++ screens/Home.js
@@ -1,42 +1,84 @@
-import React, { useCallback, useEffect, useState } from "react";
-import { View, Text, FlatList } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  FlatList,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
 import MoviePreview from "../components/MoviePreview";
-
-const URL = "https://www.omdbapi.com/?s=Marvel&apikey=eeefc96f&page=1";
 
 const Home = () => {
   const [moviesData, setMoviesData] = useState();
+  const [page, setPage] = useState(1);
 
-  const handleFetchMovies = useCallback(async () => {
-    const result = await fetch(URL);
+  const handleFetchMovies = async () => {
+    const result = await fetch(
+      "https://www.omdbapi.com/?s=Marvel&apikey=eeefc96f&page=" + page,
+    );
 
     if (result.ok) {
       const movies = await result.json();
 
       setMoviesData(movies.Search);
     }
-  }, []);
+  };
 
   useEffect(() => {
     handleFetchMovies();
-  }, []);
+  }, [page]);
 
   return (
-    <FlatList
-      //   style={styles.list}
-      data={moviesData}
-      keyExtractor={(item) => item.imdbID}
-      renderItem={({ item }) => (
-        <MoviePreview
-          onPress={() => navigation.push("MoviePreview", item)}
-          movie={item}
-        />
-      )}
-      //   refreshControl={
-      //     <RefreshControl refreshing={isRefreshing} onRefresh={handleRefresh} />
-      //   }
-    />
+    <>
+      <View style={styles.pagination}>
+        <TouchableOpacity onPress={() => setPage((page) => page - 1)}>
+          <Text style={[styles.buttonText, styles.button]}>Prev</Text>
+        </TouchableOpacity>
+
+        <Text style={styles.buttonText}>{page}</Text>
+
+        <TouchableOpacity onPress={() => setPage((page) => page + 1)}>
+          <Text style={[styles.buttonText, styles.button]}>Next</Text>
+        </TouchableOpacity>
+      </View>
+
+      <FlatList
+        style={{ flexDirection: "column" }}
+        data={moviesData}
+        keyExtractor={(item) => item.imdbID}
+        renderItem={({ item }) => (
+          <MoviePreview
+            onPress={() => navigation.push("MoviePreview", item)}
+            movie={item}
+          />
+        )}
+        //   refreshControl={
+        //     <RefreshControl refreshing={isRefreshing} onRefresh={handleRefresh} />
+        //   }
+      />
+    </>
   );
 };
 
 export default Home;
+
+const styles = StyleSheet.create({
+  buttonText: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "teal",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+    textAlign: "center",
+    height: 40,
+    width: 80,
+  },
+  pagination: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    paddingHorizontal: 40,
+    paddingVertical: 10,
+  },
+});
